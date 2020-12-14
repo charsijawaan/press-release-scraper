263179be2a83bc2079439c37acd377fe199be825
CharsiJawaan <shakeelusama3@gmail.com>
2020-12-14 14:11:07 +0500

    session setting

--- config.js
+++ config.js
@@ -1,22 +1,23 @@
 let config = {}
 config.db = {}
 
-config.webAddress = 'http://localhost:3000/'
+// config.webAddress = 'http://localhost:3000/'
+config.webAddress = 'https://alethia-app.herokuapp.com/'
 config.sessionSecret = 'sdfkjkdfjg4578945yu89'
 
 // Local Database config
-config.db.host = 'localhost'
-config.db.user = 'root'
-config.db.password = ''
-config.db.dbName = 'scraper_db'
-config.db.port = 3306
-
-// Remote Database config
-// config.db.host = 'scraper-database.cohesbi5e4hv.us-east-2.rds.amazonaws.com'
-// config.db.user = 'admin'
-// config.db.password = 'Createyour1'
+// config.db.host = 'localhost'
+// config.db.user = 'root'
+// config.db.password = ''
 // config.db.dbName = 'scraper_db'
 // config.db.port = 3306
+
+// Remote Database config
+config.db.host = 'scraper-database.cohesbi5e4hv.us-east-2.rds.amazonaws.com'
+config.db.user = 'admin'
+config.db.password = 'Createyour1'
+config.db.dbName = 'scraper_db'
+config.db.port = 3306
 
 module.exports = config
 // module.exports = sessionSecret
